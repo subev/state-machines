a3983fb72cdcbd46038cfd249d33e98f51342d81
Petur Subev <petur.subev@gmail.com>
2016-11-25 00:33:13 +0200

    add gitignore, update Gain and SM classes

--- acumulator.py
+++ acumulator.py
@@ -2,27 +2,51 @@ class SM:
     def start(self):
         self.state = self.startState
 
+    def getNextValues(self, state, inp):
+        nextState = self.getNextState(state, inp)
+        return (nextState, nextState)
+
     def step(self, inp):
         (s, o) = self.getNextValues(self.state, inp)
         self.state = s
         return o
+
     def transduce(self, inputs):
         return [self.step(inp) for inp in inputs]
 
 
 class Accumulator(SM):
-    startState = 0
-    def __init__(self, startValue):
-        self.startState = startValue
+    def __init__(self, initialValue):
+        self.startState = initialValue
 
     def getNextValues(self, state, inp):
         # returns the new state and the output
         return (state + inp, state + inp)
 
+    def run(self, n = 10):
+        return self.transduce([None]*n)
 
 
+class Gain(SM):
+    def __init__(self, k):
+        self.startState = k
+
+    def getNextValues(self, state, inp):
+        return (inp * self.state, inp * self.state)
 
 
-c = Accumulator(100)
-c.start()
-print(c.step(20))
+# ---------------------------------------
+a = Accumulator(100)
+a.start()
+print('a', a.step(20))
+print('a transduce', a.transduce([1,2,3,4,5]))
+print('a', a.step(4))
+
+a2 = Accumulator(50)
+a2.start()
+print('b', a2.step(-20))
+
+g = Gain(5)
+g.start()
+print('Gain transduce', g.transduce([3,6,5,3,2]))
+print('Gain sttep', g.step(4))
